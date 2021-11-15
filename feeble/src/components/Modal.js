8adfcaa1110f6c7025a23733075a3eb7f450ffb5
smaristeinar <smairkul@hotmail.com>
2021-11-15 18:50:11 +0100

    cool pictures and dinamic users list

--- feeble/src/components/Modal.js
+++ feeble/src/components/Modal.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Map from "./Map"
+import { useState } from 'react';
 
 function Modal(props) {
     console.log(props.info, "modal");
+    const [users, setUsers] = useState(props.info.users)
     function addUserToSession() {
         let user = localStorage.getItem("user");
         let userObj = {
@@ -20,6 +22,7 @@ function Modal(props) {
             .then(res => res.json())
             .then(data => {
                 console.log('front', data)
+                setUsers(data.participants)
 
             })
     }
@@ -32,7 +35,10 @@ function Modal(props) {
                 <h1>{props.info.city}</h1>
                 <h1>{props.info.street}</h1>
                 <h1>{props.info.date}</h1>
-                <div className="flex flex-row"><p className="font-medium">Participants:</p><ul><li className="font-normal ml-3">{props.info.users}</li></ul></div>
+                <div className="flex flex-row "><p className="font-medium">Participants:</p>
+                <ul className="flex flex-row flex-wrap">{users.map(function(name,index)
+                {return <li className="flex  flex-row font-normal ml-3" key={ index }><img className="rounded-full  m-2 h-8 w-8 flex items-center justify-center border-black border-2 filter drop-shadow-xl" src={"https://github.com/"+name+".png"} alt="profile" />{name}</li>})}</ul>
+                </div>
                 <button className=" mt-5 w-28 border rounded-xl bg-gray-200 shadow-xl font-medium" onClick={addUserToSession}>Join</button>
             </div>
         </div>
@@ -41,3 +47,5 @@ function Modal(props) {
 
 }
 export default Modal
+
+//<ul><li className="font-normal ml-3">{props.info.users}</li></ul>
