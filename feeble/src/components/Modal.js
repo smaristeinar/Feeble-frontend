28cfa3204cc53da540b065c7ef4669f86af46456
Igor Milosevic <igor.milosevic1988@gmail.com>
2021-11-15 17:05:10 +0100

    pushing

--- feeble/src/components/Modal.js
+++ feeble/src/components/Modal.js
@@ -1,38 +1,42 @@
 import React from "react";
 import Map from "./Map"
 
-function Modal(props){
+function Modal(props) {
     console.log(props.info, "modal");
-    function addUserToSession(){
+    function addUserToSession() {
         let user = localStorage.getItem("user");
-        let userObj = {userName:user}
+        let userObj = {
+            userName: user,
+            sessionId: props.info.sessionId
+        }
         console.log(user);
-        fetch("http://localhost:5000/", {
-                method: "POST",
-                headers: {
+        fetch("http://localhost:5000/session/check", {
+            method: "POST",
+            headers: {
                 "Content-Type": "application/json",
-      },
-      body: JSON.stringify(userObj)
-      })
-      .then(res => res.json())
-      .then(data => {
-        console.log(data)
-      })
-    }
-    
+            },
+            body: JSON.stringify(userObj)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log('front', data)
 
-    return(
+            })
+    }
+
+
+    return (
         <div className="p-5 m-5 bg-gray-50 bg-opacity-50 rounded-lg flex flex-row" >
-            <Map address={props.info}/>
+            <Map address={props.info} />
             <div className="ml-5">
-            <h1>{props.info.city}</h1>
-            <h1>{props.info.street}</h1>
-            <h1>{props.info.date}</h1>
-            <div className="flex flex-row"><p className="font-medium">Participants:</p><ul><li className="font-normal ml-3">{props.info.users}</li></ul></div>
-            <button className=" mt-5 w-28 border rounded-xl bg-gray-200 shadow-xl font-medium" onClick={addUserToSession}>Join</button>
+                <h1>{props.info.city}</h1>
+                <h1>{props.info.street}</h1>
+                <h1>{props.info.date}</h1>
+                <div className="flex flex-row"><p className="font-medium">Participants:</p><ul><li className="font-normal ml-3">{props.info.users}</li></ul></div>
+                <button className=" mt-5 w-28 border rounded-xl bg-gray-200 shadow-xl font-medium" onClick={addUserToSession}>Join</button>
             </div>
         </div>
-    
+
     );
 
 }
